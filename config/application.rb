82f35ffac2fdecb4028b96175bf96ce2ca6b0183
Evan Black <evan@erblack.com>
2017-02-16 16:20:50 -0600

    Initialize repository

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module FrontDoor
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set minitest spec style with no fixtures as default.
+    config.generators do |g|
+      g.test_framework :minitest, spec: true, fixture: false
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,28 @@
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+# Enable minitest
+require 'minitest/rails'
+require 'minitest/rails/capybara'
+
+# For javascript testing
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+
+# For nicer reports
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class ActiveSupport::TestCase
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
+
+# Enable Devise helpers 
+module ActionDispatch
+  class IntegrationTest
+    include Devise::Test::IntegrationHelpers
+  end
+end
