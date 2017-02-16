0c67ca2984883c14653f3dcff252df266dd0cf77
Evan Black <evan@erblack.com>
2017-02-16 16:42:30 -0600

    Add admin and pages controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable,
-         :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable,
+         :validatable
+
+  # Validations
+  validates :first_name, :last_name, presence: true
+  
+  phony_normalize :phone_number, default_country_code: 'US'
+  validates :phone_number, presence: true, phony_plausible: true
 end
