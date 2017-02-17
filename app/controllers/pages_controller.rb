31a038c4fe40a3607a7c3ce2fff599355cc2ee51
Evan Black <evan@erblack.com>
2017-02-17 00:01:08 -0600

    Add dashboard for user types

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    render "#{current_user.model_name.singular}_dashboard"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, path: '',
                      path_names: { sign_in: 'log-in', sign_out: 'log-out' }
 
+  authenticated :user do
+    root 'pages#dashboard', as: :authenticated_root
+  end
+  
   root 'pages#home'
 
   # get 'sign-up', 'clients#new'
