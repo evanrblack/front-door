b2667b35245c8a5af24775a0a0a72a7e606a24e5
Evan Black <evan@erblack.com>
2017-02-19 10:14:58 -0600

    Change header size, split out abilities

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,21 @@ class PagesController < ApplicationController
   def home; end
 
   def dashboard
-    render "#{current_user.model_name.singular}_dashboard"
+    @user = current_user
+    send("#{current_user.model_name.singular}_dashboard")
+  end
+
+  private
+
+  def admin_dashboard
+    render 'admin_dashboard'
+  end
+
+  def agent_dashboard
+    render 'agent_dashboard'
+  end
+  
+  def client_dashboard
+    render 'client_dashboard'
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,18 +2,27 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, Listing
+    
+    # Login only below
     return unless user
-
+    
     # Can manage their own account
     can %i[create read update], user
-    
-    case user.type
-    when 'Admin'
-      can :manage, :all
-    when 'Agent'
-      can %i[create read update], Listing, agent_id: user.id
-    when 'Client'
-      can %i[create read update], Listing, client_id: user.id
-    end
+
+    # Give specific abilities
+    send("#{user.model_name.singular}_abilities", user)
+  end
+  
+  def admin_abilities(admin)
+    can :manage, :all
+  end
+
+  def agent_abilities(agent)
+    can %i[create read update], Listing, agent_id: agent.id
+  end
+
+  def client_abilities(client)
+    can %i[create read update], Listing, client_id: client.id
   end
 end
